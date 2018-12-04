3fa1da7028fd21879368ee68ddf18a38aafb263f
Andrew Z Allen <me@andrewzallen.com>
2018-12-04 07:21:55 -0700

    Update iBazel to v0.8.2 (#31)
    * Update iBazel to v0.8.2
    
    * Add --verbose_failures

--- Formula/ibazel.rb
+++ Formula/ibazel.rb
@@ -15,18 +15,18 @@
 class Ibazel < Formula
   desc 'IBazel is a tool for building Bazel targets when source files change.'
   homepage 'https://github.com/bazelbuild/bazel-watcher'
-  url "https://github.com/bazelbuild/bazel-watcher/archive/v0.8.0.tar.gz"
+  url "https://github.com/bazelbuild/bazel-watcher/archive/v0.8.2.tar.gz"
   
   # To generate run:
-  # curl https://codeload.github.com/bazelbuild/bazel-watcher/tar.gz/v0.8.0 | sha256sum
-  sha256 '0596229bfe8dd70634e99e02268e0031e7f0a56e5da6dafaae3ee75dcc9f2585'
+  # curl https://codeload.github.com/bazelbuild/bazel-watcher/tar.gz/v0.8.2 | sha256sum
+  sha256 '26f5125218fad2741d3caf937b02296d803900e5f153f5b1f733f15391b9f9b4'
 
   bottle :unneeded
 
   depends_on "bazelbuild/tap/bazel" => :build
 
   def install
-    system 'bazel', 'build', '--config=release', '--experimental_platforms=@io_bazel_rules_go//go/toolchain:darwin_amd64', '//ibazel:ibazel'
+    system 'bazel', 'build', '--config=release', '--verbose_failures', '--experimental_platforms=@io_bazel_rules_go//go/toolchain:darwin_amd64', '//ibazel:ibazel'
     bin.install 'bazel-bin/ibazel/darwin_amd64_pure_stripped/ibazel' => 'ibazel'
   end
 
