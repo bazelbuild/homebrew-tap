c444db92eaa1f43dedb27550cd6c9fb44ce15296
Florian Weikert <fwe@google.com>
2019-02-18 17:52:26 +0100

    Fix indentation.

--- Formula/bazelisk.rb
+++ Formula/bazelisk.rb
@@ -13,26 +13,25 @@
 # limitations under the License.
 
 class Bazelisk < Formula
-    desc 'Bazelisk is a user-friendly launcher for Bazel.'
-    homepage 'https://github.com/philwo/bazelisk'
-    url "https://github.com/philwo/bazelisk/releases/download/v0.0.1/bazelisk-darwin-amd64"
+  desc 'Bazelisk is a user-friendly launcher for Bazel.'
+  homepage 'https://github.com/philwo/bazelisk'
+  url "https://github.com/philwo/bazelisk/releases/download/v0.0.1/bazelisk-darwin-amd64"
     
-    # To generate run:
-    # curl -L -N -s https://github.com/philwo/bazelisk/releases/download/v0.0.1/bazelisk-darwin-amd64 | shasum -a 256
-    # on MacOS
-    sha256 'c5da312c70a8540766cd275bdac46e1cc14ab29c3282f70e64af4e046a0a99bc'
-  
-    bottle :unneeded
-  
-    conflicts_with "bazelbuild/tap/bazel", :because => "Bazelisk replaces the bazel binary"
-  
-    def install
-      bin.install 'bazelisk-darwin-amd64' => 'bazel'
-    end
-  
-    test do
-      # Simply run bazelisk to see whether it finished
-      system bin / 'bazel', 'version'
-    end
+  # To generate run:
+  # curl -L -N -s https://github.com/philwo/bazelisk/releases/download/v0.0.1/bazelisk-darwin-amd64 | shasum -a 256
+  # on MacOS
+  sha256 'c5da312c70a8540766cd275bdac46e1cc14ab29c3282f70e64af4e046a0a99bc'
+
+  bottle :unneeded
+
+  conflicts_with "bazelbuild/tap/bazel", :because => "Bazelisk replaces the bazel binary"
+
+  def install
+    bin.install 'bazelisk-darwin-amd64' => 'bazel'
   end
-    
+
+  test do
+    # Simply run bazelisk to see whether it finished
+    system bin / 'bazel', 'version'
+  end
+end
